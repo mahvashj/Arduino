b871e75614754ab0e7d17d4056da500c61c5f7ad
mjafarin2064 <mjafarin@gmail.com>
2018-03-06 15:46:19 -0800

    vibr_FW.ino
    Arduino based vibration Firmware with piezoelectric sensor.

---
+++ vibr_FW.ino
@@ -0,0 +1,15 @@
+int vPin = 9; 
+int frValue = 0;  // stores the current feed value
+int dirVar = 1; // stores the direction of the variation
+
+void setup() {
+}
+
+void loop() {
+  
+  // test current value and change the direction if required
+  if (frValue == 0 || frValue == 255) dirVar *= -1;
+  
+  analogWrite(vPin, frValue + dirVar);
+  delay(30);
+}
