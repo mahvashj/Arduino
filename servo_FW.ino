d9370b9e86eadcbd9b37c60edb84e8970ed2c6b4
mjafarin2064 <mjafarin@gmail.com>
2018-03-06 16:08:34 -0800

    servo_FW
    zero - 180 servo controller formware

---
+++ servo_FW.ino
@@ -0,0 +1,24 @@
+#include <Servo.h>
+
+Servo theServo;  
+int angle = 0;  
+
+void setup() 
+{ 
+  theServo.attach(9);  
+} 
+
+
+void loop() 
+{ 
+  for(angle = 0; angle < 180; angle += 1)   
+  {                                  
+   theServo.write(angle);               
+    delay(20);                       
+  } 
+  for(angle = 180; angle >= 1; angle -=1)     
+  {                                
+    theServo.write(angle);              
+    delay(20);                       
+  } 
+}
