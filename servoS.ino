214f417874071646c869c29964a050271de3424b
mjafarin2064 <mjafarin@gmail.com>
2018-03-06 16:26:33 -0800

    servoS
    Running multiple servos with a single power source

---
+++ servoS.ino
@@ -0,0 +1,27 @@
+#include <Servo.h> 
+
+Servo servo1;
+Servo servo2;
+Servo servo3;
+
+int angle;
+
+void setup() 
+{ 
+  servo1.attach(9);
+  servo2.attach(10);
+  servo3.attach(11);
+} 
+
+void loop() 
+{ 
+  for(angle = 0; angle < 180; angle += 1)   
+  {                                  
+    servo1.write(angle);
+    servo2.write(145-angle/2);
+    servo3.write(180-angle);
+
+    delay(25);                       
+  }
+}
+
